9b80bfdc8c20967988e639a563c92fbbb8749f26
nabilsaragih <nabilsaragih33@gmail.com>
2024-06-01 02:00:39 +0800

    fix: Ciri Dzolim: Suka Mengambil Hak Orang Lain

--- src/main/java/org/example/rpsfxgl/Multiplayer.java
+++ src/main/java/org/example/rpsfxgl/Multiplayer.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-<<<<<<<< HEAD:src/main/java/org/example/rpsfxgl/Multiplayer.java
 public class Multiplayer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -15,14 +14,6 @@ public class Multiplayer extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
         stage.setTitle("Destiny Clash: Deluxe Edition");
-========
-public class Reglog extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Reglog.class.getResource("LoginScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Game RPS");
->>>>>>>> 5ffb036b6a2649003a45bf78bc018edcf6b7d1d9:src/main/java/org/example/rpsfxgl/Reglog.java
         stage.setScene(scene);
         stage.show();
     }
